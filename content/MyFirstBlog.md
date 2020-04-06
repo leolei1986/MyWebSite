cfd57424246b25dba2f9fd5bdac6273f1239cb0d
Leo <cherishl163@163.com>
2020-04-06 12:39:42 +0800

    Finished first commit

---
+++ content/MyFirstBlog.md
@@ -0,0 +1,12 @@
+Title: 我的第一篇文章
+Date: 2020-04-06
+Modified: 2020-04-06
+Category: 日志
+Tags: 日志
+Author: 雷舰
+Status: published
+
+# 标题1
+## 标题2
+完成第一篇上传的文章，留个纪念。
+[导航](search.html)
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,19 +3,21 @@
 from __future__ import unicode_literals
 
 AUTHOR = '雷舰'
-SITENAME = '我的博客'
+SITENAME = '我的站点'
 SITEURL = ''
 
 PATH = 'content'
 
 TIMEZONE = 'Asia/Shanghai'
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = 'en'#'zh'
 THEME = "../pelican-themes/pelican-bootstrap3"
+BOOTSTRAP_THEME = "flatly"
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 PLUGIN_PATHS = ['../pelican-plugins']
 PLUGINS = ['i18n_subsites']
 #I18N_TEMPLATES_LANG = 'en'
+LOAD_CONTENT_CACHE = False
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -23,17 +25,25 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+DELETE_OUTPUT_DIRECTORY = True
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Pelican', 'http://getpelican.com'),
+         ('Python.org', 'http://python.org'),
+         ('Bootswatch', 'https://bootswatch.com'),
+         ('Jinja2', 'http://jinja.pocoo.org'),
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('My Github', 'https://github.com/leolei1986'),
+        ('My CSDN blog', 'https://blog.csdn.net/cherishlei'),
+        )
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+#static paths will be copied without parsing their contents
+#STATIC_EXCLUDE_SOURCES = False
+STATIC_PATHS = [
+        'search.html',
+        ]
